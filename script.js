999524006d2845af87927c49d5fb6d53c6e694bf
Thizh <s22010229@ousl.lk>
2024-01-09 17:43:57 +0530

    modified:   script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 window['webgl-fluid'].default(document.querySelector('canvas'));
 document.querySelector('.wrapper')
   .addEventListener('mousemove', event => {
-    console.log('move');
     newEvent = new event.constructor(
       event.type, event);
     document.querySelector('canvas')
       .dispatchEvent(newEvent);
 });
-document.querySelector('button')
-  .addEventListener('click', event => {
-    console.log('button clicked');
+
+document.querySelector('.wrapper')
+  .addEventListener('touchmove', event => {
+    newEvent = new event.constructor(
+      event.type, event);
+    document.querySelector('canvas')
+      .dispatchEvent(newEvent);
 });
